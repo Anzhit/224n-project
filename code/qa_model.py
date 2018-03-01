43512033b83efe0955445e5c8a240ebb7ca216b0
Anchit Gupta <anchit.post@gmail.com>
2018-02-28 17:33:40 -0800

    Add a linear layer

--- code/qa_model.py
+++ code/qa_model.py
@@ -147,9 +147,9 @@ class QAModel(object):
 
         attn_layer = ComplexAttn(self.keep_prob, self.FLAGS.hidden_size*2, self.FLAGS.hidden_size*2)
         _, attn_output = attn_layer.build_graph(question_hiddens, self.qn_mask, context_hiddens,"q2cAttention") # attn_output is shape (batch_size, context_len, hidden_size*2)
-        # blended_reps = tf.contrib.layers.fully_connected(attn_output, num_outputs=self.FLAGS.hidden_size*2,activation_fn=None) # blended_reps_final is shape (batch_size, context_len, hidden_size)
-        # blended_reps=tf.contrib.layers.layer_norm(blended_reps+context_hiddens)
-        blended_reps=tf.concat([context_hiddens,attn_output],axis=2)
+        blended_reps = tf.contrib.layers.fully_connected(attn_output, num_outputs=self.FLAGS.hidden_size*2,activation_fn=None) # blended_reps_final is shape (batch_size, context_len, hidden_size)
+        blended_reps=tf.contrib.layers.layer_norm(blended_reps+context_hiddens)
+        blended_reps=tf.concat([context_hiddens,blended_reps],axis=2)
         # Concat attn_output to context_hiddens to get blended_reps
         
         # blended_reps = tf.concat([context_hiddens, blended_reps], axis=2) # (batch_size, context_len, hidden_size*4)
