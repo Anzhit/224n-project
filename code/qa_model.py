3af8051b45bb49ba3e2ed2991309a5108815b78e
Anand Dhoot <anandd@stanford.edu>
2018-03-12 19:22:45 -0700

    LR scheduling

--- code/qa_model.py
+++ code/qa_model.py
@@ -72,7 +72,12 @@ class QAModel(object):
         # Define optimizer and updates
         # (updates is what you need to fetch in session.run to do a gradient update)
             self.global_step = tf.Variable(0, name="global_step", trainable=False)
-            opt = tf.train.AdamOptimizer(learning_rate=FLAGS.learning_rate) # you can try other optimizers
+            
+            # Exponential decay
+            starter_learning_rate = FLAGS.learning_rate
+            learning_rate = tf.train.exponential_decay(starter_learning_rate, self.global_step, 750, 0.96, staircase=True)
+            
+            opt = tf.train.AdamOptimizer(learning_rate=learning_rate) # you can try other optimizers
             self.updates = opt.apply_gradients(zip(clipped_gradients, params), global_step=self.global_step)
 
         # Define savers (for checkpointing) and summaries (for tensorboard)
